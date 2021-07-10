f67309a368f5614ddc909866c71ca26d872b484e
XFaon <85759101+XfaonAE@users.noreply.github.com>
2021-07-10 17:31:05 -0400

    Enabled compiler cache

---
+++ src/PlexiCore.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var PlexiCore = /** @class */ (function () {
+    function PlexiCore() {
+    }
+    return PlexiCore;
+}());
+exports.default = PlexiCore;
